d0766b4eb4fec13577bdd1262ec8a8d44fc2ce69
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2024-11-14 19:36:48 +0100

    🎨 Add final changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { ComingSoonComponent } from './views/coming-soon/coming-soon.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'Nirvana-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    ComingSoonComponent,
+    FooterComponent,
+  ],
+  template: ` <main class="main">
+      <nirvana-header></nirvana-header>
+      <nirvana-coming-soon></nirvana-coming-soon>
+      <nirvana-footer></nirvana-footer>
+    </main>
+    <router-outlet />`,
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'NirvanaApp-Angular';
