a5c1da8dcc26cd6cedacea59376c3f01fd30e0fb
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2024-11-15 19:04:38 +0100

    Add routes and initial home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
-import { ComingSoonComponent } from './views/coming-soon/coming-soon.component';
 import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'Nirvana-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    HeaderComponent,
-    ComingSoonComponent,
-    FooterComponent,
-  ],
-  template: ` <main class="main">
-      <nirvana-header></nirvana-header>
-      <nirvana-coming-soon></nirvana-coming-soon>
-      <nirvana-footer></nirvana-footer>
-    </main>
-    <router-outlet />`,
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  template: `
+    <div class="app">
+      <nirvana-header />
+      <main class="main">
+        <router-outlet />
+      </main>
+      <nirvana-footer />
+    </div>
+  `,
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { ComingSoonComponent } from './views/coming-soon/coming-soon.component';
+import { HomeComponent } from './views/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'coming-soon' },
+  {
+    path: 'coming-soon',
+    title: 'Coming Soon',
+    loadComponent: () => ComingSoonComponent,
+  },
+  {
+    path: 'home',
+    title: 'Home',
+    loadComponent: () => HomeComponent,
+  },
+];
